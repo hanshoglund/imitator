930071146c1aafe4f3a3226ed4803be91a20d3c5
Hans Höglund <hans@hanshoglund.se>
2013-03-27 17:41:15 +0100

    Switched to score repr (for commands)

--- src/Score.hs
+++ src/Score.hs
@@ -7,40 +7,38 @@ module Score (
 
 import Music.Score
 import Music.Imitator
+import Data.Semigroup
 import Control.Monad
 import Control.Concurrent (threadDelay)
-
+import Data.VectorSpace
+import Data.AffineSpace
 
 
 
 -- FIXME duration must be shorter than env start time
 
-mainScore :: Track Command
-mainScore = join $ Track [
-    -- (0,   return $ StartRecord)
-    (0,     return $ ReadBuffer "/Users/hans/Desktop/Test/Test1loud.aiff"),
-    (0,     sp1),
-    (10,    sp1),
-    (19,    sp1),
-    (26,    sp2),
-    (34,    sp2)
-    -- (300,   return $ StopRecord)
-    ]
+mainScore :: Score Command
+mainScore = mempty
+    |> (note $ ReadBuffer "/Users/hans/Desktop/Test/Test1loud.aiff")
+    |> sp2^*10
+    |> sp2^*10
+    |> sp2^*8
+    |> sp1^*8
+    |> sp2^*8
 
-sp1 = Track [
-    (0.0,   PlayBuffer nd 50 7 0.4 3 (0    * tau)),
-    (0.2,   PlayBuffer nd 50 7 0.4 3 (-0.2 * tau)),
-    (0.4,   PlayBuffer nd 50 7 0.4 3 (-0.2 * tau)),
-    (0.6,   PlayBuffer nd 50 7 0.4 3 (0    * tau))
-    ]
+sp1 = mempty
+    |> rest^*0.0 |> (note $ PlayBuffer nd 50 7 0.4 3 (0    * tau))
+    |> rest^*0.2 |> (note $ PlayBuffer nd 50 7 0.4 3 (-0.2 * tau))
+    |> rest^*0.4 |> (note $ PlayBuffer nd 50 7 0.4 3 (-0.2 * tau))
+    |> rest^*0.6 |> (note $ PlayBuffer nd 50 7 0.4 3 (0    * tau))
 
-sp2 = Track [
-    (0.0,   PlayBuffer nd 80 7 0.4 3 (0    * tau)),
-    (0.2,   PlayBuffer nd 80 7 0.4 3 (0.2  * tau)),
-    (0.4,   PlayBuffer nd 80 7 0.4 3 (-0.2 * tau)),
-    (0.6,   PlayBuffer nd 80 7 0.4 3 (0.4  * tau)),
-    (0.8,   PlayBuffer nd 80 7 0.4 3 (-0.4 * tau))
-    ]
+sp2 = mempty
+    |> rest^*0.0 |> (note $ PlayBuffer nd 80 7 0.4 3 ((1      ) * tau))
+    |> rest^*0.2 |> (note $ PlayBuffer nd 80 7 0.4 3 ((1 + 0.2) * tau))
+    |> rest^*0.4 |> (note $ PlayBuffer nd 80 7 0.4 3 ((1 - 0.2) * tau))
+    |> rest^*0.6 |> (note $ PlayBuffer nd 80 7 0.4 3 ((1 + 0.3) * tau))
+    |> rest^*0.8 |> (note $ PlayBuffer nd 80 7 0.4 3 ((1 - 0.3) * tau))
+
 
 nd = 0
 
@@ -63,14 +61,18 @@ nd = 0
 
 
 
+
 main :: IO ()
-main = do
+main = rt
+
+nrt = do
     writeSynthDefs
-    runImitatorNRT mainScore
-    
-    -- startServer
-    -- threadDelay 1000000
-    -- runImitatorRT mainScore
+    runImitatorNRT (scoreToTrack mainScore)
+
+rt = do 
+    startServer
+    threadDelay 1000000
+    runImitatorRT (scoreToTrack mainScore)
 
 tau = pi * 2
 
