d3a256ddc1678972bd4b238720224ad5f03406b6
Hans Höglund <hans@hanshoglund.se>
2013-04-09 23:11:05 +0200

    More drawing

--- src/Score.hs
+++ src/Score.hs
@@ -450,7 +450,7 @@ simple = id
 --------------------------------------------------------------------------------
 
 main :: IO ()
-main = rt
+main = drp
 
 -- |
 -- Ad-hoc drawing of commands notes.
@@ -483,11 +483,63 @@ drawScores notes cmds = notes1D <> notes2D <> cmdsD <> middleLines <> crossLines
         getD = fromRational . toRational
         getP = (subtract 60) . fromIntegral . getPitch
 
+reset x = moveBack (offset x .-. 0) x
+-- |
+-- Ad-hoc drawing of commands notes.
+--
+drawPoster :: Diagram SVG R2
+drawPoster = mconcat $ fmap (\(i,a) -> translate (rotate (tau*a::Rad) $ r2 (0,z)) $ alignB $ rotate (tau*a::Rad) $ alignX 0 $ alignY 0 $ notes i) $ zip [0,1..] $ [0,(1/n)..1]
+    where     
+        notes i = alignTR $ mconcat $ fmap (drawNote 1) $ perform $ ns (i `mod` 4)
+                                                                               
+        
+        n  = 8
+        z  = 100
+        dr = 40
+        ns v = (takeS dr $ (!! v) $ voices $ rev canon_IV)
+
+        drawNote n (t,d,x) =
+                drawNote' n (t,d,x)
+                 <> drawFigure n (t,d,x)
+        
+        drawFigure n (t,d,x) = id
+                $ translateY (getP x-30) 
+                $ translateX (getT (t.+^(d)))  
+                $ lw 0.2
+                $ alignTL
+                $ lcA transparent
+                $ fcA (black `withOpacity` 0.2) 
+                -- $ vrule (fromIntegral $ 2 * (round $ getTime $ t) `mod` 4)
+                $ rect 0.4 (fromIntegral $ 2 * (round $ getTime $ t) `mod` 4)
+
+
+        drawNote' n (t,d,x) = id
+                $ translateY ((getP x-30)) 
+                $ translateX (getT (t.+^(d^/2))) 
+                $ scaleX (getD d) $ noteShape n
+
+        noteShape 1 = id
+                $ lcA transparent
+                $ fcA (black `withOpacity` 0.2) 
+                $ square 1
+        noteShape 2 = lcA transparent $ fcA (green `withOpacity` 0.3) $ square 1
+
+        totalDur = getD $ dr{-duration ns-}
+        getT = fromRational . toRational
+        getD = fromRational . toRational
+        getP = (subtract 60) . fromIntegral . getPitch  
+
 dr = do
-    let sc  = drawScores noteScore cmdScore
-    let svg = renderDia SVG (SVGOptions (Dims 1800 800)) sc
+    let dia = drawScores noteScore cmdScore
+    let svg = renderDia SVG (SVGOptions (Dims 1800 800)) dia
     let bs  = renderSvg svg
     ByteString.writeFile "score.svg" bs
+
+drp = do
+    let dia = drawPoster
+    let svg = renderDia SVG (SVGOptions (Dims 1800 800)) dia
+    let bs  = renderSvg svg
+    ByteString.writeFile "poster.svg" bs
 
 nrt = do
     writeSynthDefs
@@ -650,6 +702,6 @@ off :: Score ()
 off = rest
 
 
-
+tau = pi*2
 
 
